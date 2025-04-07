96eab7d249c83cf1de0ddb23463ca4fcdde86065
jose712020 <150953639+jose712020@users.noreply.github.com>
2025-04-07 23:43:00 +0200

    Add files via upload

--- src/persistencia/Persistencia.java
+++ src/persistencia/Persistencia.java
@@ -7,14 +7,19 @@ import models.Trabajador;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class Persistencia {
+
+    public static final String RUTA_P = "config/config.properties";
+
     // Metodo que lee el producto de los catalogos
     public static ArrayList<Producto> leeCatalogo() {
         ArrayList<Producto> productos = new ArrayList<>();
-        File carpetaProductos = new File("data/productos");
+        File carpetaProductos = new File(leeRutaProductos());
 
         if (!carpetaProductos.exists()) carpetaProductos.mkdirs();
+        if (carpetaProductos.list() == null) return productos;
 
         for (String archivo : carpetaProductos.list()) {
             try {
@@ -34,7 +39,7 @@ public class Persistencia {
 
     // Metodo que guarda los producto de los catalogos en disco
     public static void guardaCatalogoEnDisco(ArrayList<Producto> catalogo) {
-        File carpetaProductos = new File("data/productos");
+        File carpetaProductos = new File(leeRutaProductos());
 
         if (!carpetaProductos.exists()) carpetaProductos.mkdirs();
 
@@ -46,9 +51,10 @@ public class Persistencia {
     // Metodo que lee los administradores
     public static ArrayList<Admin> leeAdmins() {
         ArrayList<Admin> admins = new ArrayList<>();
-        File carpetaAdmins = new File("data/usuarios/administradores");
+        File carpetaAdmins = new File(leeRutaAdmins());
 
         if (!carpetaAdmins.exists()) carpetaAdmins.mkdirs();
+        if (carpetaAdmins.list() == null) return admins;
 
         for (String archivo : carpetaAdmins.list()) {
             try {
@@ -68,7 +74,7 @@ public class Persistencia {
 
     // Metodo que guarda los administradores en disco
     public static void guardaAdminsEnDisco(ArrayList<Admin> admins) {
-        File carpetaAdmins = new File("data/usuarios/administradores");
+        File carpetaAdmins = new File(leeRutaAdmins());
 
         if (!carpetaAdmins.exists()) carpetaAdmins.mkdirs();
 
@@ -95,7 +101,7 @@ public class Persistencia {
 
     // Funcion que busca el ID del trabajador de prueba
     private static boolean buscaTrabajadoresPrueba() {
-        File carpetaTrabajadores = new File("data/usuarios/trabajadores");
+        File carpetaTrabajadores = new File(leeRutaTrabajadores());
 
         if (!carpetaTrabajadores.exists()) carpetaTrabajadores.mkdirs();
 
@@ -115,7 +121,7 @@ public class Persistencia {
 
     // Funcion que busca el ID del cliente de prueba
     private static boolean buscaClientePrueba() {
-        File carpetaClientes = new File("data/usuarios/clientes");
+        File carpetaClientes = new File(leeRutaClientes());
 
         if (!carpetaClientes.exists()) carpetaClientes.mkdirs();
 
@@ -138,9 +144,10 @@ public class Persistencia {
     // Metodo que lee los clientes en disco
     public static ArrayList<Cliente> leeClientes() {
         ArrayList<Cliente> clientes = new ArrayList<>();
-        File carpetaClientes = new File("data/usuarios/clientes");
+        File carpetaClientes = new File(leeRutaClientes());
 
         if (!carpetaClientes.exists()) carpetaClientes.mkdirs();
+        if (carpetaClientes.list() == null) return clientes;
 
         for (String archivo : carpetaClientes.list()) {
             try {
@@ -161,9 +168,10 @@ public class Persistencia {
     // Metodo que lee los clientes en trabajadores
     public static ArrayList<Trabajador> leeTrabajadores() {
         ArrayList<Trabajador> trabajadores = new ArrayList<>();
-        File carpetaTrabajadores = new File("data/usuarios/trabajadores");
+        File carpetaTrabajadores = new File(leeRutaTrabajadores());
 
         if (!carpetaTrabajadores.exists()) carpetaTrabajadores.mkdirs();
+        if (carpetaTrabajadores.list() == null) return trabajadores;
 
         for (String archivo : carpetaTrabajadores.list()) {
             try {
@@ -183,7 +191,7 @@ public class Persistencia {
 
     // Metodo que guarda los clientes en disco
     public static void guardaClientesEnDisco(Cliente cliente) {
-        File carpetaClientes = new File("data/usuarios/clientes");
+        File carpetaClientes = new File(leeRutaClientes());
 
         if (!carpetaClientes.exists()) carpetaClientes.mkdirs();
 
@@ -200,7 +208,7 @@ public class Persistencia {
 
     // Metodo que guarda los trabajadores en disco
     public static void guardaTrabajadorEnDisco(Trabajador trabajador) {
-        File carpetaTrabajador = new File("data/usuarios/trabajadores");
+        File carpetaTrabajador = new File(leeRutaTrabajadores());
 
         if (!carpetaTrabajador.exists()) carpetaTrabajador.mkdirs();
 
@@ -216,8 +224,9 @@ public class Persistencia {
         }
     }
 
+    // Metodo que guarda los productos en disco
     public static void guardaProductoEnDisco(Producto producto) {
-        File carpetaProductos = new File("data/productos");
+        File carpetaProductos = new File(leeRutaProductos());
 
         if (!carpetaProductos.exists()) carpetaProductos.mkdirs();
         try {
@@ -228,6 +237,54 @@ public class Persistencia {
             fos.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    // Metodo que lee la ruta de los productos
+    public static String leeRutaProductos() {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new BufferedReader(new FileReader(RUTA_P)));
+            return prop.getProperty("RUTA_PRODUCTOS", "data/productos");
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    // Metodo que lee la ruta de los administradores
+    private static String leeRutaAdmins() {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new BufferedReader(new FileReader(RUTA_P)));
+            return prop.getProperty("RUTA_ADMINISTRADORES", "data/usuarios/administradores");
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    // Metodo que lee la ruta de los trabajadores
+    public static String leeRutaTrabajadores() {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new BufferedReader(new FileReader(RUTA_P)));
+            return prop.getProperty("RUTA_TRABAJADORES", "data/usuarios/trabajadores");
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+    // Metodo que lee la ruta de los clientes
+    public static String leeRutaClientes() {
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new BufferedReader(new FileReader(RUTA_P)));
+            return prop.getProperty("RUTA_CLIENTES", "data/usuarios/clientes");
+        } catch (IOException e) {
+            return "";
         }
     }
 }
